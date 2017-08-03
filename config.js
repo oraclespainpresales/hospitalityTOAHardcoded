9b2f690342f01e889c94d95a8cbf084afcafffe6
carlos_picazo_sordo <carlos.picazo.sordo@oracle.com>
2017-08-03 11:34:14 +0200

    fix

--- config.js
+++ config.js
@@ -3,10 +3,10 @@ var path = require('path');
 module.exports = {
   server: {
     port: 19091,
-    securePort: 19092,                                 
+    securePort: 19092,
     cookieSecret: 'etadirect-tools',
     distFolder:  __dirname,
     staticUrl: __dirname,
-  	ip: '0.0.0.0',
+  	ip: '0.0.0.0', 
   }
 };
