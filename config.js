ba3422ff94b221b1bef536e2e588384fc89b810a
carlos_picazo_sordo <carlos.picazo.sordo@oracle.com>
2017-08-03 11:55:48 +0200

    changing ports

--- config.js
+++ config.js
@@ -2,11 +2,11 @@ var path = require('path');
 
 module.exports = {
   server: {
-    port: 19091,
-    securePort: 19092,
+    port: 19991,
+    securePort: 19992,
     cookieSecret: 'etadirect-tools',
     distFolder:  __dirname,
     staticUrl: __dirname,
-  	ip: '0.0.0.0', 
+  	ip: '0.0.0.0',
   }
 };
